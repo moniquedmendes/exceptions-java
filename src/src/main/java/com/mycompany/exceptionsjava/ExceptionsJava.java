06c72652eea870611f6a30b7ec99ce44fb1642ff
Monique Mendes <111323380+moniquedmendes@users.noreply.github.com>
2025-07-22 19:48:19 -0300

    Add files via upload

---
+++ src/src/main/java/com/mycompany/exceptionsjava/ExceptionsJava.java
@@ -0,0 +1,42 @@
+
+package com.mycompany.exceptionsjava;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import model.entities.Reservation;
+
+
+public class ExceptionsJava {
+
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        System.out.println("Room number: ");
+        int number = sc.nextInt();
+        System.out.println("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.println("Check-Out date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+        
+        if(!checkOut.after(checkIn)){
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }else{
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        }
+        
+        System.out.println("");
+        System.out.println("Enter data to update the reservation: ");
+        System.out.println("Check-in date (dd/MM/yyyy): ");
+        sdf.parse(sc.next());
+        System.out.println("Check-Out date (dd/MM/yyyy): ");
+        sdf.parse(sc.next());
+        
+        reservation.updateDates(checkIn, checkOut);
+        
+        sc.close();
+    }
+}
